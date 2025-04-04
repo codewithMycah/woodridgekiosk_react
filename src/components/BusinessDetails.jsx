f453231acce1b88e2bb04a116d9ee31229c38c5a
Mycah Accad <56968035+codewithMycah@users.noreply.github.com>
2025-04-03 20:49:54 -0500

    added 404

--- src/components/BusinessDetails.jsx
+++ src/components/BusinessDetails.jsx
@@ -3,6 +3,9 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 import config from '../config';
 
+// icons
+import { MapPin } from 'lucide-react'
+
 export default function BusinessDetails() {
 
     const { business_id } = useParams();
@@ -13,27 +16,34 @@ export default function BusinessDetails() {
         const fetchData = async () => {
             try {
                 const response = await axios.get(`${config.API_URL}/business/${business_id}`)
-                console.info(response.data)
+                // console.info(response.data)
 
                 setBusiness(response.data)
             } catch (error) {
-                console.error('Error:', error.message)
+                console.error('Error:', error.error)
             }
             };
         
         fetchData();
     }, [business_id]);
 
+    // Helper to get image URL
+    const getImageUrl = (image) => `${config.API_URL}/image/${image}`;
+
     if (!business) {
-        return <div>Business not found!</div>;
+        return <div className='flex h-screen items-center justify-center text-3xl font-bold'>Business not found!</div>;
     }
 
     return (
         <>
             {business && (
-                <div>
-                    <h1>{business.business_name}</h1>
-                    <h1>{business.business_category}</h1>
+                <div className="hero bg-cover bg-center h-80 text-white" style={{ backgroundImage: `url(${getImageUrl(business.image)})` }}>
+                    <div className="bg-linear-to-t from-black/50 to-black/10 h-full w-full">
+                        <div className='flex flex-col items-start justify-end min-w-4xl lg:w-5xl xl:w-7xl mx-auto pb-8 h-full'>
+                            <h1 className='text-3xl font-bold'>{business.business_name}</h1>
+                            <h3 className='flex flex-row gap-2'><MapPin />{business.address}</h3>
+                        </div>
+                    </div>
                 </div>
             )}
         </>
--- src/components/Businesses.jsx
+++ src/components/Businesses.jsx
@@ -10,7 +10,7 @@ import { jsonquery } from '@jsonquerylang/jsonquery'
 import config from '../config';
 
 // icons
-import { MapPin, Phone, Link as LinkIcon, ArrowUpAZ } from 'lucide-react'
+import { MapPin, Phone, Link as LinkIcon, ArrowUpAZ, ListFilter } from 'lucide-react'
 
 export default function Businesses() {
     const [businesses, setBusinesses] = useState([]); // variable to store the data from backend
@@ -80,9 +80,7 @@ export default function Businesses() {
                     <div className="flex px-4 py-1 rounded-md border-2 border-green-500 overflow-hidden max-w-lg mx-auto">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192.904 192.904" width="16px"
                             className="fill-gray-600 mr-3 rotate-90">
-                            <path
-                                d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z">
-                            </path>
+                            <path d="m190.707 180.101-47.078-47.077c11.702-14.072 18.752-32.142 18.752-51.831C162.381 36.423 125.959 0 81.191 0 36.422 0 0 36.423 0 81.193c0 44.767 36.422 81.187 81.191 81.187 19.688 0 37.759-7.049 51.831-18.751l47.079 47.078a7.474 7.474 0 0 0 5.303 2.197 7.498 7.498 0 0 0 5.303-12.803zM15 81.193C15 44.694 44.693 15 81.191 15c36.497 0 66.189 29.694 66.189 66.193 0 36.496-29.692 66.187-66.189 66.187C44.693 147.38 15 117.689 15 81.193z"></path>
                         </svg>
                         <input 
                             type="text"
@@ -94,8 +92,14 @@ export default function Businesses() {
                 </div>
                 <div>
                     {/* Sort Button */}
-                    <button className='flex flex-row items-center justify-center gap-2 px-4 py-1 rounded-lg border-2 border-gray-300' onClick={btnSort}>
+                    <button className='cursor-pointer flex flex-row items-center justify-center gap-2 px-4 py-1 rounded-lg border-2 border-gray-300 hover:scale-105 hover:border-green-500' onClick={btnSort}>
                         <ArrowUpAZ /> Sort
+                    </button>
+                </div>
+                <div>
+                    {/* Filter Button */}
+                    <button className='cursor-pointer flex flex-row items-center justify-center gap-2 px-4 py-1 rounded-lg border-2 border-gray-300 hover:scale-105 hover:border-green-500' onClick={btnSort}>
+                        <ListFilter /> Filter
                     </button>
                 </div>
             </div>
